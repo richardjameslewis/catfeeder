e990b0d307c3f1781c9cde88619a13a1a406170b
Richard Lewis <richard@jmpax.demon.co.uk>
2020-01-08 21:05:21 +0000

    New version. Now closed loop.

--- catfeeder.py
+++ catfeeder.py
@@ -2,43 +2,46 @@ import sys
 import time
 import RPi.GPIO as GPIO
 from enum import Enum
+from feeder import Feeder
 
 # Constants
 PWM_FREQUENCY = 50
 # BOARD numbers for outputs
-NALA_PWM_PIN = 35
-ROSIE_PWM_PIN = 12
-NALA_TIME = 1.25      # 1.2
-ROSIE_TIME = 1.45     # 1.5
-#ROSIE_TIME = 1.0     # 1.5
-FLIP_LEFT = (2.0/20.0) * 100
-FLIP_RIGHT = (1.0/20.0) * 100
-LEFT_FLIP_TIME = 0.4
-RIGHT_FLIP_TIME = 0.35
-FLIP_DELAY = 0.2
-#WIGGLE_LEFT = (2.0/20.0) * 100
-#WIGGLE_RIGHT = (1.0/20.0) * 100
-#WIGGLE_COUNT = 2
-#WIGGLE_TIME = 0.3
+# Nala left
+LEFT_RESET_PIN = 12   # 32
+LEFT_DATA_PIN = 16    # 36
+LEFT_PWM_PIN = 19     # 35
+LEFT_ADC_CHAN = 0
 
-# Vegetables...sorry, variables
+# Rosie right
+RIGHT_RESET_PIN = 24  # 18
+RIGHT_DATA_PIN = 25   # 22
+RIGHT_PWM_PIN = 18    # 12
+RIGHT_ADC_CHAN = 1
+
 nala = None
 rosie = None
 
 def main(argv):
-  print("Cat Feeder 0.2")
+  print("Cat Feeder 0.3")
   argc = len(argv)
 
-  if argc != 2 or argv[1] == "--help":
+  if argc == 2 and argv[1] == "--help":
     help()
-  elif argv[1] == "--init":
+  elif argc == 2 and argv[1] == "--init":
     init()
-  elif argv[1] == "--left":
+  elif argc == 2 and argv[1] == "--cal":
     init()
-    feed(MotorState.LEFT)
-  elif argv[1] == "--right":
+    cal()
+  elif argc == 3 and argv[1] == "--feed":
     init()
-    feed(MotorState.RIGHT)
+    feed(float(argv[2]))
+  #elif argv[1] == "--left":
+  #  init()
+  #  feed(False)
+  #elif argv[1] == "--right":
+  #  init()
+  #  feed(True)
   else:
     help()
 
@@ -48,164 +51,44 @@ def main(argv):
 
 def help():
   print("--init - Initialize the GPIO pins for the cat feeder")
-  print("--left - Feed the cats. Start turning left.")
-  print("--right - Feed the cats. Start turning right.")
+  print("--cal - Calibrate the cat feeder, needs 250g of food loaded into feeder.")
+  print("--feed N - Feed N grams of food")
+  #print("--left - Feed the cats. Start turning left.")
+  #print("--right - Feed the cats. Start turning right.")
   print("--help - Print this message")
-
-#class Stage(Enum):
-#    WIGGLE1 = 0
-#    PAUSE1 = 1
-#    FLIP1 = 2
-#    PAUSE2 = 3
-#    WIGGLE2 = 4
-#    PAUSE3 = 5
-#    FLIP2 = 6
-#    PAUSE4 = 7
-#    COMPLETE = 8
-#    START = 9
-
-
-class MotorState(Enum):
-    LEFT = 0
-    LEFTWAIT = 1
-    RIGHT = 2
-    RIGHTWAIT = 3
-    COMPLETE = 4
-    START = 5
-#    RIGHTWIGGLE = 6
-#    LEFTWIGGLE = 7
-
-stateparams = {
-    MotorState.LEFT:      { "cycle": FLIP_LEFT, "duration": LEFT_FLIP_TIME},
-    MotorState.LEFTWAIT:  { "cycle": 0, "duration": FLIP_DELAY },
-    MotorState.RIGHT:     { "cycle": FLIP_RIGHT, "duration": RIGHT_FLIP_TIME },
-    MotorState.RIGHTWAIT: { "cycle": 0, "duration": FLIP_DELAY },
-    MotorState.COMPLETE:  { "cycle": 0, "duration": 0 },
-    MotorState.START:     { "cycle": 0, "duration": 0 },
-#    MotorState.LEFTWIGGLE: { "cycle": WIGGLE_LEFT, "duration": WIGGLE_TIME },
-#    MotorState.RIGHTWIGGLE: { "cycle": WIGGLE_RIGHT, "duration": WIGGLE_TIME },
-}
 
 def init():
   global nala, rosie, startstate
 
-  GPIO.setmode(GPIO.BOARD)
+  #GPIO.setmode(GPIO.BOARD)
+  GPIO.setmode(GPIO.BCM)
   GPIO.setwarnings(False)
   startstate = None
-  nala = HungryCat(NALA_PWM_PIN, NALA_TIME, FLIP_LEFT, FLIP_RIGHT)
-  rosie = HungryCat(ROSIE_PWM_PIN, ROSIE_TIME, FLIP_LEFT, FLIP_RIGHT)
+  # nala = Feeder("nala", 25, 6.2, LEFT_PWM_PIN, LEFT_RESET_PIN, LEFT_DATA_PIN, LEFT_ADC_CHAN)
+  nala = Feeder("nala", LEFT_PWM_PIN, LEFT_RESET_PIN, LEFT_DATA_PIN, LEFT_ADC_CHAN)
+  #rosie = Feeder(RIGHT_PWM_PIN)
 
-def feed(state):
-  global startstate
-  startstate = state
+def feed(weight):
+  nala.startFeed(weight)
+  #rosie.startFeed(100)
+  nala.join()
+  #rosie.join()
+  if nala.empty:
+    print("Warning: Nala's feeder is empty")
 
-  nala.startFeed()
-  rosie.startFeed()
-  while True:
-    n = nala.feed()
-    r = rosie.feed()
-    if n == 0 and r == 0:
-      break
-    elif n == 0:
-      time.sleep(r)
-    elif r == 0:
-      time.sleep(n)
-    else:
-      time.sleep(min(n, r))
-
-class HungryCat:
-  def __init__(self, pin, total_time, left, right):
-    self.PIN = pin
-    self.TOTAL_TIME = total_time
-    self.LEFT_CYCLE = left
-    self.RIGHT_CYCLE = right
-    self.motorstate = MotorState.LEFT
-    self.next_time = 0
-    self.time_left = 0
-    self.count = 0
-
-    GPIO.setup(self.PIN, GPIO.OUT)
-    self.pwm = GPIO.PWM(self.PIN, PWM_FREQUENCY)
-    self.pwm.start(0)
-
-  def startFeed(self):
-    self.motorstate = MotorState.START
-
-  def done(self):
-    if self.time_left == 0:
-      return True
-    if self.motorstate == MotorState.LEFT or self.motorstate == MotorState.RIGHT:
-      t = time.time()
-      return self.time_left <= (t - self.last_time)
-    return False
-
-  def feed(self):
-
-
-    return self.doMotor()
-
-  def doMotor(self):
-    if self.motorstate == MotorState.COMPLETE:
-        return 0
-
-    t = time.time()
-    print("feed {0}".format(t));
-
-    if self.motorstate != MotorState.START and self.done():
-      print("{0} Complete".format(self.PIN))
-      self.motorstate = MotorState.COMPLETE
-      self.pwm.ChangeDutyCycle(0)
-      return 0
-
-    if t >= self.next_time or self.motorstate == MotorState.START:
-      adjustTime = False
-      if self.motorstate == MotorState.START:
-        self.motorstate = startstate
-        self.time_left = self.TOTAL_TIME
-        #self.motorstate = MotorState.LEFTWIGGLE
-        #self.wiggle_count = WIGGLE_COUNT
-        #self.time_left = self.TOTAL_TIME + WIGGLE_TIME * WIGGLE_COUNT * 2
-        print("Start {0}".format(self.motorstate))
-      #elif self.motorstate == MotorState.LEFTWIGGLE:
-      #  self.motorstate = MotorState.RIGHTWIGGLE
-      #  adjustTime = True
-      #elif self.motorstate == MotorState.RIGHTWIGGLE:
-      #  self.wiggle_count = self.wiggle_count - 1
-      #  self.motorstate = startstate if self.wiggle_count == 0 else MotorState.LEFTWIGGLE
-      #  adjustTime = True
-      elif self.motorstate == MotorState.LEFT:
-        self.motorstate = MotorState.LEFTWAIT
-        adjustTime = True
-      elif self.motorstate == MotorState.LEFTWAIT:
-        self.motorstate = MotorState.RIGHT
-        #self.motorstate = MotorState.LEFT
-      elif self.motorstate == MotorState.RIGHT:
-        self.motorstate = MotorState.RIGHTWAIT
-        adjustTime = True
-      else:              # RIGHTWAIT
-        self.motorstate = MotorState.LEFT
-        #self.motorstate = MotorState.RIGHT
-
-      if adjustTime:
-        self.time_left -= t - self.last_time
-        if self.time_left < 0:
-           self.time_left = 0
-
-      #print("MotorState {0}".format(self.motorstate))
-      #print("Params {0}".format(stateparams))
-      #print("Param {0}".format(stateparams[self.motorstate]))
-      #print("Cycle {0}".format(stateparams[self.motorstate]["cycle"]))
-
-      cycle = stateparams[self.motorstate]["cycle"]
-      delta = stateparams[self.motorstate]["duration"]
-      print("{0} MotorState change to {1} cycle {2}".format(self.PIN, self.motorstate, cycle))
-      self.pwm.ChangeDutyCycle(cycle)
-      self.last_time = t
-      self.next_time = t + delta
-
-    ret = min(self.time_left, self.next_time - t)
-    print("{0} returning {1}".format(self.PIN, ret))
-    return ret
-
+def cal():
+  sums = 0
+  # remove any excess
+  nala.resetSettings()
+  # feed in big chunks (possibly a mistake)
+  nala.resetCalibration()
+  while not nala.empty:
+    nala.startFeed(25)
+    nala.join()
+    sums += nala.sums
+    #time.sleep(5)
+  nala.calibrate(250, sums)
+  nala.resetSettings()
+  
 if __name__ == "__main__":
     main(sys.argv)
